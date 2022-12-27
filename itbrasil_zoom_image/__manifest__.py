7b00b358d08022adb32a3a083f2867faf16c5b82
Filipe Nogueira Silveira <fnosilveira@gmail.com>
2022-12-27 12:03:51 -0300

    add: iniital commit

---
+++ itbrasil_zoom_image/__manifest__.py
@@ -0,0 +1,20 @@
+
+{
+    'name': 'Zoom Image Product',
+    'version': '14.0..0.1',
+    'summary': 'Zoom Image Product',
+    'description': 'Zoom Image Product',
+    'category': 'Extra Tools',
+    'author': 'IT Brasil',
+    'maintainer': 'IT Brasil',
+    'company': 'IT Brasil',
+    'website': 'https://www.itbrasil.com.br',
+    'depends': ['base'],
+    'data': [
+        'views/zoom_image.xml',
+        
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
